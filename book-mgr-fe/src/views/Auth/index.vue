9a76da88d065339d308003c6ca879a49ad0ae51a
fanaticworker <fanaticworker>
2022-04-06 17:53:04 +0800

    这是测试

---
+++ book-mgr-fe/src/views/Auth/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="auth">
+        <div class="title-info">
+         <img src="" alt="">
+        <div class="title">图书管理系统 后台</div>
+     </div>
+
+      <div class="form">
+        <a-tabs>
+        <a-tab-pane key="1" tab="登入">
+            login  
+        </a-tab-pane>
+        <a-tab-pane  key="2" tab="注册">
+             register
+        </a-tab-pane>
+        </a-tabs>
+      </div>
+    </div>
+</template>
+<script src="./index.js"></script>
+<style lang="scss" scpoed>
+@import './index.scss';
+
+</style>
